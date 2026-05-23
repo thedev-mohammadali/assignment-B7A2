cb6de591c3825dcecf2e7b2207e226aee71e1e6c
Mohammad Ali <thedev.mohammadali@gmail.com>
2026-05-23 23:47:09 +0600

    Remove await from server.ts for initDB

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { initDB } from "./db";
 const PORT = config.port;
 
 const main = async () => {
-  await initDB();
+  initDB();
   app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
   });
