1122ee1acd753d826559deb35eb7d72aca47f8ca
Mohammad Ali <thedev.mohammadali@gmail.com>
2026-05-23 23:22:25 +0600

    fix: Change env object to process.env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,13 @@
 import { configDotenv } from "dotenv";
 import type { SignOptions } from "jsonwebtoken";
 
-const env: Record<string, string | undefined> = {};
-
-configDotenv({ processEnv: env, quiet: true });
+configDotenv({ quiet: true });
 
 const config = {
-  port: Number(env.PORT) || 5000,
-  connectionString: env.DATABASE_URL as string,
-  secret: env.SECRET as string,
-  tokenExpiresIn: (env.EXPIRY as SignOptions["expiresIn"]) || "1d",
+  port: Number(process.env.PORT) || 5000,
+  connectionString: process.env.DATABASE_URL as string,
+  secret: process.env.SECRET as string,
+  tokenExpiresIn: (process.env.EXPIRY as SignOptions["expiresIn"]) || "1d",
 };
-
-// console.log(config.secret);
 
 export default config;
