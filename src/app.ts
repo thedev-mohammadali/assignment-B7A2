6ccba3d5b6a3db34c9b59041034f91749b47fbad
Mohammad Ali <thedev.mohammadali@gmail.com>
2026-05-23 22:23:19 +0600

    Add tsup.config.ts for deployment

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,18 @@ import express, { type Application } from "express";
 import { globalErrorHandler } from "./middleware/globalErrorHandler";
 import { authRoute } from "./modules/auth/auth.route";
 import { issueRoute } from "./modules/issues/issue.route";
+import { sendResponse } from "./utils/sendResponse";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  sendResponse(res, 200, {
+    success: true,
+    message: "Welcome to DevPulse",
+  });
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/issues", issueRoute);
